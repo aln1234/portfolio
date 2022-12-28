2e541ae8be7d6bd07970557c0e5e642098a6b746
Alvin Lamichhane <39218356+aln1234@users.noreply.github.com>
2022-12-28 23:33:00 +0200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
           </div>
         </div>
         <div className=" w-56 cursor-pointer rounded-full hover:shadow-lg hover:scale-105  transition duration-500 ">
-          <img src="https://scontent.fktm7-1.fna.fbcdn.net/v/t1.6435-9/78101211_2555839957818249_4531610901563834368_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_ohc=Hoe8g4N84XkAX8xHJWO&_nc_ht=scontent.fktm7-1.fna&oh=00_AT920RPC4oMNnb6Xw0POabXy4K3r_muvAVhtxLoKN10vRw&oe=62188BBD" 
+          <img src="https://lh3.googleusercontent.com/SKZnsTtOdvmErmRUCBoXq8qQsp7iyHf2mDxT4n0gV30XffZbWI7qZgLvUNQFf2JcqSg1yVngrlzjPjlVR2El-bl-uvPfkyCaZsyi5papqwlwfwedImOoncue1h1qb6TsOIoYyHwob70EE1XSFIfflSs4Bp6lVIb9BNU9QMlu31Jt61IYaAiJ4quyo635pi9VGOotwmwb4Yy0aOPbkC-WRzF-GRLnmFrrDrhiEVgOdHPdlgbjalkK-dJqY72qX7sovMBpwgfKQiLwCunHUy9OjEO9-cyL4uys34__YQ4uCXQM74TZm8AuKOtTMl13CEFpsdpej3-SH9KmHwLs8xoxp-vt0I-GNfjPTBLxNTLZYRoN-ACBbIcTLB4zIaY_YI0r71yxmCXC93_imgjWrgtAwnJerc3G061JDCj6Pc1gDPIFnjs3kTQK4V-ylS-fWR15RUvvfm6UF5bhdGnPwu7iV0SjSkGo-SS7GgPQN4flza5ZKHkEMwsVgSjOxy-N6k3eP4SZ0q9uStYazMGz-BRVEbZHOJfNG7tgP4WhOSvJFRUDS6vln-NcMAYRQE5-m0lJkkJpFaVRhg2wIDdO3MxCabnD_fvsL6gQirWMJzpXF2y9D6g3fnaRqX8c7nvM4PUjdER0Y7Pq9lNg13Odkb5dM-F3Kkg62VFbgP9EyZ6Fv_xMYDqZ4TBlvvYPgzmHMdR6H7F8ARxVFz7wCG0mqAtPlOHACF8Q901f6bWqdVMdMyLtgghD-xxiM32lvRDWpWBE5CWYwFaBNcPak9f7fOVGAGNkRwk-AqPA5hlSuwaJQ-0__MtmTq-UKXfnIg8vzPI_C-iMiHkBebav2kr0izmSWpCy3UWKwxFQV0tc3_GraJAS8wV9RekqdAdvWyfgTR9sjB6UuGb_9gn8jjYmKcREv_Pqi973aSlp9YbuTcZ3FZ4LBusq=w216-h220-no?authuser=0" 
           alt="Profile Pic" 
           loading="lazy"
           className="rounded-full"/>
