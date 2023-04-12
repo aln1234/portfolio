35d8c1e988be87a6f4bb122801cc37f0ba7e5870
Albin Lamichhane <albin.lamichhane@tuni.fi>
2023-04-12 20:36:06 +0300

    added new project voice gpt, added local images

--- components/section/Work.js
+++ components/section/Work.js
@@ -8,7 +8,7 @@ const Work = () => {
     <>
       <div className="grid md:grid-cols-2 md:gap-10">
       <Card
-          src="/assets/image/voice.png"
+          src="https://lh3.googleusercontent.com/kN8yswqcyegTuarzAQ2tmKOIE2zlqhHkM9tu360C_2krkXLgE2uN992nYhTGwKNpfgbbPtBmnuHacJA8Gnem8L_yogIKquWgfn7TEOIhJFysqMeJSoro5XrRoepzXAGZOrWXOwN4djXKqbayvL9HiaiiQ7PCTYH6MoGsem1sjU0DWMkFxgkqPPmDTWQ8OZe9pJAhLMYZDOb7sNigmrHsKOJvLMWXN-RhY7nHVDYMrB5WCbC8_Bn8fsxBhvbxl-ybXYkpp8LCmknjBb_eHL2u0drx7acqSDH4t7Zf6GS2bBWJOeAjuCOvyeq2TK5ABwVXocJLqvmsXANDdopaUgWFOXm6dT2P2lej7iIglyLNPyZJABzjxV-VCkks5og-iNA5Qi3s51oRx6WPXTbuMaO7x-d63mmFCYUGoqdmDzMqCQu_u0e0z1o9TTP1EbIrLOTcngyVpnAOVFvk2k-BQEp3HxjkhrWfnEWUCpEqTaH9MncAkmzLJt00UeMAOUQ3kCaJVyIgLG5DEUc9j7l0oCfi6O00ungl1uvq4XS10bOq8RsgbvX2fv2lspgMPed7nykE_OmbgW9JdU5Hk4VXDKZgQs-D_Cf1nnvR3usnKs8UtQPeOCyCx8VQhLIUOIu2ABkKiAVtDdmZsxNbUs4SHKi5XGHyFepIjQgItPnp83tyLvxJtD51kMx_3UvOZMXjQ20qQY9w63QfCjNvb8vcjb0GUQkE-FmNd1ymbfOd5ZXQjIDWx6_OHW9CRtFCxupIYl5JSbcukY8SgVzzk-odRShd-m0b-_RPZXkawnVpaHj6oHUocooSRA0Hr_GZ9fFRrhZPz-F88ZKEsWQIkIHAPfrDH52xATqtVvwbByMoyYIBcHilt6yoNBK8CT57J_MF_b8H2xde_wIMMTjemcf4-ktNfcuh6-973SU79bt3QHo-6hjoN9JZW0Wmgmn5htJgBnvKsN2_UwBLFi2p14un9GgexQ=w1893-h943-s-no?authuser=0"
           alt="Voice GPT"
           link="https://voicegpt-omega.vercel.app/"
           title="Voice GPT"
@@ -16,7 +16,7 @@ const Work = () => {
           description="In this project user can use their voice to send prompt to Chat Gpt and get results"
         />
         <Card
-         src="/assets/image/restore.png"
+         src="https://lh3.googleusercontent.com/TKGKXm97X6elotbltX5RuvFUhBDTF0D7N5QHEs50PSMFceknWSAH8GWNKjpVqwuctCBF5UmfBUm4rXMtntFSbuhE_f6tfhGOfvw1qAz52voZRncek7XVJN5kj4EjJYHhQBKB5kq3_a2NL1JlpkiK06l03DYnPETzK7yBLMiadK1jv6Sx4LAHFWfDeGC36Vm5SkouQgcfTn4x7GHsDNx-Ai4q8xE80CeZi43LOKNMxAXBFH5vmJ0MUannXD-dmUt6cCrxw1gLcrU2q_kKcg_vfYIqdime3IZLle2lXHzvLDNEKH6nsxPPUZgLde0lhYKwGd8NZRrUuc00fWgh-d6GgAv0uQmi-hFXMIIU-pkb85ACwe4Tyidn1B9r_F3CYzlN6_sghDvuxMpDGjh6dsj1iw_fkV6TEeOoTYuAqrBzZAdReDWiG50oXY2MvhVXU5apIjhaR-AInQvaNEKYo6hpNvYy-zpBq5ddtAf2vj_Sr8GLU7gtFFNIgiLyjdecCViKMjaFaEuWUM7co16i7tzCVbvZffBpmSJO6dp5rGPxgAkPZbD4uQ8qZ2EoQvnnfDcLeWvho83RUtc0Ucn1WDsTLOHINOxlkiNH4KdTluVKZO9QfEoz-vIvCeihkX9soXR0yidHIuBR1DNf1ZYOwXzbYRJw-sGAM1P4b5Zybw0CvbXdRbTTL7vLjN5mUQhVJ3n_ugNr3Y21VGV43pXncj-n0g7WF6JFCPdDYJHRtoZRhKYbpcT4l6SPiB6q0X8ZNsH31RnxIwPrdaIv5xP7QwhLM35HMYbrOVPQM9AHgyWPA9rPcw1ePVd4GXgp4Hm0FbCiPpoceWHZsLi9VNX4nwQmwya9BBF-dB0Wy3fZwHF0v9Sww1trLKKutu7W7XwEcmrUR_7sT730z7_UkH3uNzmjH0DrHcf7JZzfuQ8SQgoCQ1rZO0RMs5Nev96SKthQml8X4742O19WeafGq0oZrq6qtg=w1920-h935-s-no?authuser=0"
           alt="Restore Photo"
           link="https://restorephoto.vercel.app/"
           title="Restore Photo"
@@ -24,7 +24,7 @@ const Work = () => {
           description="This is my first time implementing a machine leraning project where user can restore any blur photo using Artificial Intelligence"
         />
         <Card
-          src="/assets/image/two.png"
+          src="https://lh3.googleusercontent.com/qDRt-didafLHntxltZvpm8cGhNjQEDA617I9_X1qCefABw_1u5kWDwTp74sCVG5b3xUds3beB4OmhwA7VxBRpbBNbyAcSNi2yt0LxpRCW52-9XdLlEs3DffzlPiHNVCUNuAQxfr9eEg7jnqroxydvHKr0jfxmLEjAP4ktCRIhNpRiYyS7Vaw296g1AJjiFvG1RI5gPvlW5FdW0gVcjCVRbn20KqXc_IkeP0-ma6RAkeoNdfUjqr1McNlfBpfyW9T3ikxQVo7ODdxx2aXMnE3u-95Dy08VGtaFD62CrkcSUkvg59QIhAVpuwgA_D2l3XW0EA_rujyH43F8Hjv2npQ1Y0dRsEaCR5fJ8n6k8EWcLSl8Db8Ehee3u4gKuV3xXtjjACYOlBjTd3GE5UlhPnC2bwtF9nhkXfAboc6iKvowmdetI_kx6gXOGDWzR-GzLOsgp6nA10ED3HL0vpsPftLqpxNrCIc5g1HPo9KRaJ-06aZTm__wlcNmTa7fSrM3nR3l7hwGIb2VBjSNgOktx8stcPj6NV6xodbmvE4Q9BWqVLGiwrd1SGZGMs7PSpYdWIG-IGOnrmlXGfm4KF_1Oil3KWdTisT8qDGtwFdS6YJa4cOMj2p1z-eZOceCpzMB07yIyN5aSo0FM9qvKVaVFbhOqt5OrWic4bAbgvaAS6Xh7BDALnPnMVxT0PfK80WlRh5FnjkOeNefsSZhdY9f0F72exZ_spjm01hYYuvNBuRxmvhJsqpHOh3BP4zL3bE_emuC7wUM1O8LTgs8qN3o7E2Q-yeuK2pSFPcSs3BvsCuWOQQvYaUjZMmxBhQWNYFqHNRB7ebTE_3K5WXGhH66pggqsI7YTFLfB2g_2MDyPxoVGQFiKROhMpIpDbt_67vE0k9zZ8LKKkJImkhjzDTQPHJwoT8_MCyHkHqGa9LRhkVNX7FFoJUgy8XoDpgU7zgbcb36ElUkPJtrAdYEUsix8Sngg=w948-h410-no?authuser=0"
           alt="Webscraping image"
           link="https://thekruathai.com/menu/"
           title="Kruthai Restaurant"
@@ -32,7 +32,7 @@ const Work = () => {
           description="A free-lancing project for a Thai Restaurant"
         />
         <Card
-          src="/assets/image/three.png"
+          src="https://lh3.googleusercontent.com/QfLjPL4AC04OGN63JqCIBojPV82JYu8GbO9cL8d9-qxb6Jg6XnfTsXG4VytEgyFYRdzHadyd7NnKn2mo4pxgFcN3MrKXk0v8cNC6y1DVkvXPzTkuX2U1oTJFtv-Q0nBQ8z5QuKxZujrlNboDXBLWjYrSGErj02ORTUBLPUijxi-IKqerqFoKND036W5MQV3ojA2I4sgD8hB-_JnMx0j7vNU3qlFu9l1WIlGzpLfNFO0-BCqzwF6HfTIkJ0XUF9tCdPumZVTh1ZYDaXVLJYu4cj_h2QUqQrOmdB2SUhSyNIle0s3aIiX2M-rtDYcRCQtuiasWPqCFI2vSaWtAQG9-9nOJ3cUFnzbs4cmPtXa91N7JRDds_PB4bK7pPMbKHhWcVqk6q1i4GlDh7ziyGHcr468Ra_TwqAMzfyM9ZfvYf3Tcq0wYZkV3ZY-1vD3vVdVULgEkeu_-up2yk4Itg6W9mWx47tYjLiSuWbcXSHTXJq-_BCkUg_J6xKDYFVnVuhfnJ_ijScR54mm7TEIN_Dsg-C98kXteyn2Exi5w5P0s5660yGjhqkUKKUDTrditOcbkQhKtiZUpSC1HJ5J5mI5sploPLsBgTEnf-i2EwcmfB2tYL2tbEALYyMSO5r935aXyQgJR2bbr4HWGbl5PfeHGZjZ_leZguFfi6QFrAfUQoEnT8dibqueWgicDh6Vf7WpPN10uJVoZ3E3wJARZ5s5b1074JTfDKWcj3jOfb2CTqku54Hoa-9K6gumlO4vx7McvLx9BgFI-MFCBoUYTQ8gCzvgfTnUH3UuG-w0gBnwEF3ZucrW6hYa2J-9NAFerE26FGE8sjGqYcKIz-SdLIef9EA51boidZP8_hebpMrrFEF8ViSmkCxkON470VFku00nqGS28QlSVx3prHTjSAcDyEty8Gsc6shK-_k1krl28WsTVfeAEQKqsuCNMuAdWNy_TTQkncFD9esejr36plYNEJg=w1920-h795-s-no?authuser=0"
           alt="A ecoomerce site"
           link="https://hajurbuwa.com/"
           title="Ecommerce Website"
@@ -40,7 +40,7 @@ const Work = () => {
           description="A ecommerce site that let you buy and sell goods"
         />
         <Card
-          src="/assets/image/four.png"
+          src="https://lh3.googleusercontent.com/99qjX36QEo5J2_HvBHXpCNzB22Xj_2jywSclr5QjlWSCiDFcXyzlmWaHvrprNABLabyYUKrvErXloFCOKh5RB15RF-OSjktgyxXPusm9UUGgAJdPYaIuL6fbH5Ru-tRwjenmzWXxk_FX9exub0H_VPtfGHICd1fVkF7EG86v0H9kiwgaNJ54kU9Ao47O2WvNuQYOmxoF_fEmi7d3A1cU1SxCIgSnb63O_IvJNXzqMBwN4GLpE78h0SyLxTiqiRj3FUJi60e2sRO4kAAARgSOtLRRPwhnplzkGHujYOFBkUewe62TF55m0McxgnMaHjD-B_S08ZavlXUHK63Yz-rAEXFkL44f4omz1zUksq11hI-NpLnUayjnHAb0-cm946cHwAsKs-6GW0juh8pBt8y27oj1eYcxIbRPS8_BGRPis8stgkXnI9M2ltuyPPaFiiPyBA8EesKMNi1m0t14DZEfOtdhg9G_na4G029tHSB7VYV-WK-xtqv__7_v1ZKZA6CC8TjkElVMaNcFRNxhTvHPOJWxc8AXTQ9X9kgrMSdIdYuT9fBLeOaixkp2OD6e8WHBLPZ7UDRswsienS99h1HEMjrDBeQXA-BVuw8_BCK0n7gQG5e73UbDfZLq1sYafs3j1zznW4cTrzMLuQj5UW9h8mvLL6GLAUugckAuC_AbQL6AmJ0LDMSt6s31_59raGr1PaV7m9IWv68w6gQ60yd-eP4EURoYeF3w2GG1VZvcV3lJ5lETzdImBFzPsjMw_MXsFq9s1hYJAETb3KZlA_2BfV8AHwUYWZKALcEw-BwoqERFygvVhgZqpdJB7UJ9CFPkmNjcKO6T86W5iLWRjbFDwfgUoBDfhzEQ6qKnkL2kbFctDWz5BsFT2X-egDadfFK_5mFNre5VBjRvGWj9_HV9M_xJ1L2tnNyaksaDBVjBcEj7PTnYWbori_2UkVPXNyk0zqE4joEb2DibL-KVU7faOg=w1920-h832-s-no?authuser=0"
           alt="Personal Project"
           link="https://disney-clone-odu3ui157-albinlamichhane9-gmailcom.vercel.app/"
           title="Github.io Portfolio"
