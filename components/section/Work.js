d176fd25a4917e58767457ccd6a6c1294a188ad6
Albin Lamichhane <albin.lamichhane@tuni.fi>
2023-04-12 20:22:28 +0300

    added new project voice gpt, added local images

--- components/section/Work.js
+++ components/section/Work.js
@@ -7,16 +7,24 @@ const Work = () => {
   return (
     <>
       <div className="grid md:grid-cols-2 md:gap-10">
-        <Card
-          src="https://lh3.googleusercontent.com/pw/AMWts8CeUmBqhjlD4Z1UdI349g_yMnhhtbvm009J94a5IbynOtFmUJ-0fxlZDXlqeTBXYs3OWc6LxtfJLRuV5t8WjaVfOze1bYM694_WAQ1l99Rozesygu6JwWi-Q_Tux1XIIBpIQl6rOQylPhpGaE2rp5e0S0VCYzrohDcoxVvSducXbECvViM5IgAp5r-9kMW3j7VcypSgbISwEi28y4_QEiDGLf7Xkp3YzZ3Ewl883cy8hcwE-Pr9J5aRXuap13AUvo0-oCXwBWdYCR52LjH44l7jhuEdKFR5mLlKc8OKQOMFBBrTFjM-5dlU-Tq5-BiYnNV4_deqypbqe9eGow2C5bZSiEZ1_zKCZzwoKTriF2-xX2OoIcNHM4dn10aiScIMKvCK3-iTFESqbTaNaKTgcXEb7FGy2__UsYJu_Bgy1J2oh54hHSHj58ZqBsjAcJjinwuA9C7ElyQuCleUxZzlRVsOlUTT1CvwfqhsgD2-hcwnwLBy2hZqaE50-CnHWkIwee_3mkJIZtnvka4B4lZmOkvaXXAhC4Sv0QLiqBfrKQL-vJ2E2RCxOLrQ1cvUD-ijjAElN55ELK5ySLnQ1-H5JLHFIUWRHZAZCnLd3__hbMaHx-3_zXGs1sIYepuvTW38UDKlVO09S2wz6xQAm6Pe5OxCNVdgfAEVjAWxm5NGLb7iaPWM_3Je1txuPW3lAQ1Cjhf1MKDfT6fvszrcUIi4Q4e_koONCKgm9akjMSrDDxMn9RtjRaHtuA-sJxYgp6tzVX4gQYnDPUtmWQ9GZBhDPxqGSO6oXAXYRhWSJj-Id_ViyyTowGQwNFenGbSoT2iswU3KvT-obnzygcqUDx3bbejwW8-4LV40RaRi29LIIl_LSfkKJ5sZ-cOWyv_LHhEdLTGc3Ti2-30si-so4yCEH4Nd96UfO6vJCl7-HZhObIDnQzCPFS3IkXSnIfC1CiVW-L_BbgEpwr1GJBFkPz_ehUuprkX63FDKSFm36At6Qdo=w400-h194-no?authuser=1"
-          alt="Restore Photo"
-          link="https://restorephoto.vercel.app/"
-          title="Company Profile Site"
+      <Card
+          src="\assets\image\voice.png"
+          alt="Voice GPT"
+          link="https://voicegpt-omega.vercel.app/"
+          title="Voice GPT"
           date="Feb 18"
-          description="This is my first freelance project displaying information related to a company."
+          description="In this project user can use their voice to send prompt to Chat Gpt and get results"
         />
         <Card
-          src="https://lh3.googleusercontent.com/zKIr0LfQQinEJBD20eCAUOttTAeYIbypJJiZyRN_KkEhPUrOoVRoI3K8xRcPDdJrdWc7OoEpcI08jZm-l0eyqgBmfy4379FWm5YlX1sehgiUEVbEz_wCr4PUTe297dRXuedWBUGZ-Z5Mwy3Yyy0A4kf8TM4YcUo-a6he97HcC3YqW8E3GEIzJL7XzDJtfAy-kyW-BK_Nd0zfLPZuatp26j9Wg1yoq0vtrS1Wc1Gzhzn8MXhCWRzFAl_j7gQoYHbVmAzIh_ytdTzmHctovxmE7g3TFAyQ14QiKfakr6829bCwnRxocAlx6wMbPeNsQlo_iBhEyy-N9-uSVXQrK0tngu87hUAt8SW1aaTYuqHvGSQcZonVW7oQ0RiQcj-Hv923XyhvD-WuiNFg4pc7YY2tNMmFJa_ZqU6tnNTZg5UEhMTwk5niuxOCLajcDueozT4NMfo3TYxTyKh_9YcWZkNeT3p0YgOk--4OzqqI0FzjyY3z57I_YZqTzABwUjaPVJfhYVKBVquLXH0crwy0U9MLc32hIOFczbL-JGDgLj3PYVDnq8EmZpVEfDslGYev67QASebK6yFBnA8icg0l0yZYS1m1o70o4krBI3QP07O7TqGdYIMHuupA7oFg3Zz5AM6VlTq6RpHmiPRqThNzSIL4KgHttfiN9Hl4hQ7IRrkKY29-HJdQYAcwJuJbvNcmw9Ws0OLTOH4Dhv7Qkg0Ge4TOC3KK=w1920-h830-no?authuser=0"
+         src="\assets\image\restore.png"
+          alt="Restore Photo"
+          link="https://restorephoto.vercel.app/"
+          title="Restore Photo"
+          date="Feb 18"
+          description="This is my first time implementing a machine leraning project where user can restore any blur photo using Artificial Intelligence"
+        />
+        <Card
+          src="\assets\image\two.png"
           alt="Webscraping image"
           link="https://thekruathai.com/menu/"
           title="Kruthai Restaurant"
@@ -24,7 +32,7 @@ const Work = () => {
           description="A free-lancing project for a Thai Restaurant"
         />
         <Card
-          src="https://lh3.googleusercontent.com/0wWHkwyUKrKNvoX0esedn4phRBLgEVJec4Vv077IRLMpjUUYN_m_1AKRx60e3vy5L5i2MiZzZdIdGlUqIZqzkW25JlNiCtZKGbFgeMSmHD7MtUzX7ZVBSlLt78BVmYSL654lq3dINwJKpDJsAlaqbzCutJQ8m5HLJkg2mNY9iueA9l9ymT603B1h4e92r7AkpLY4cz4WzZUlnRizUNYRpulwEIeCy9vd_U3z9Q7llD9tCOb8WUQlNrIyC39vmTSYxa_bxc0MNMEzcZ6cQtpVgleeulJ3lqLINmZCq9e8naVs8rEdPWZj2XgkhMgpp4MtKOeCQUIU3SXWEZHKBLA2-11YqzR9yUoYEEDl0VV04B0YQRnNFl5DlOpX-1BzKSIIh16D1Q6PwkNOHM9ZR7NLSlDxT_9lUSDi_FOfD4I0yxNREOIkNWTVoN-UPtGEsqfOLT5UdYa1_Qz3QE11aDyseoXls1pNfI8Ft1tp1tOF9KOXUySgbhkSKsLhwZSdoR58_IQabWt1EM06vgiz9bmIYu91dp0bs0YMgoD26EMZJ5q0kpOW4CZ_zvSe8_--DKTBqRe0V38UHNTQE4q8DokZbN_9jaFPiSIfzC8Khgfy-4qTw-S7OfQWwdfmC7xcgD6gxwukFVN0W4U9m4MuS_pZLIDA0mGSb6Ylz4uLBdT1i_1VMqn4-2SYj71jXsN82fDnmzktMz1-qtgjcSr4kagXS08Q=w1920-h795-no?authuser=0"
+          src="\assets\image\three.png"
           alt="A ecoomerce site"
           link="https://hajurbuwa.com/"
           title="Ecommerce Website"
@@ -32,7 +40,7 @@ const Work = () => {
           description="A ecommerce site that let you buy and sell goods"
         />
         <Card
-          src="https://lh3.googleusercontent.com/hIDkw-L-t1lG5nxZGbYiBqvLUvJAIf7s_XgP12rRwTN7NYqeHHueNO00VwOURes6PMP8D5XfcAdkkaqypIpVWS_MZgj3AFe_UEibRLfmAh3B0pQCPN6pkcHQaTLlIr5yDgvxPsu5TPNkzNJKZmrMoFI40PWyYS2CEWF_jx78w9G1j8ERk3N8csTPPDhubAfssGWu8vptlOSpMy_2q4Xe5wvb0SOjjB7IFfh8mntQLvn_iPYRELV3zsqwEZoc-lqMd5v9PIBuYDgh64mT-LXowe5bPorYtoS9RFm79wk3ntSEKMRDwJgxQOXxOXoa9RV8-g0-23se0203Je036yXUnPwT0TizMSJuxHlipOayH4cm1RMkgbUPKkljye8F_8vJI7Q59oX_xMbVZXXBXQBR0weX9psSm2kDkjA1XxmLA6c3ChIeCwKpiEFrk7l6bYhFnDPxsKw_YK8QRuRQKAOfgXpipkh_awOSX46CHPq3hZrdJGbdqmiBrFiV0DhD8FOUwgIj2HCNKr8_GyvdWUczEUwtBnEzIKPyNjr1HAguapAQ_w3ps1AtIWUelfQx6YwGAsGn7sAikEwHZvE0GKqXLSM3EGyi3yOmHBLt2siNE6njxPR852BUMZw3tqNvGyimSbviwgicSuWo9AMIl3eENgLuMgCrg5FBDbrFu7ZZe7bOe-XKznAVJenvbQYn-o6yH3RB2x5YaSLu41E-bi0OpmQz=w1917-h830-no?authuser=0"
+          src="\assets\image\four.png"
           alt="Personal Project"
           link="https://disney-clone-odu3ui157-albinlamichhane9-gmailcom.vercel.app/"
           title="Github.io Portfolio"
